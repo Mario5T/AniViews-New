6ed7c196cc84b8622f12048a4f4f004b4353685e
Aditya Singh <singhtiivu@gmail.com>
2025-03-27 19:59:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,33 @@ import GenreDetails from './components/GenreDetails';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { FaFire, FaStar, FaCalendarAlt } from 'react-icons/fa';
-import { BrowserRouter as Router } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Hero />} />
+      <Route path="trending" element={<TrendingAnime />} />
+      <Route path="seasonal" element={<SeasonalAnime />} />
+      <Route path="genres" element={<AnimeGenres />} />
+      <Route path="genre/:id" element={<GenreDetails />} />
+      <Route path="anime/:id" element={<AnimeDetails />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <Router>
-      {/* Your routes here */}
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
